519fa4f6300f5dc2a18e36c9a6469eb74dbe70f0
jangsam24 <jangsam24@gmail.com>
2024-10-11 16:48:37 +0900

    10/11 main_views에서 게시판 경로 추가함.

--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -4,7 +4,7 @@ from werkzeug.utils import redirect
 bp = Blueprint("main", __name__, url_prefix="/")
 
 
-# 기본 페이지
+# 일정표
 @bp.route("/")
 def board_page():
     return redirect(url_for("schedule._list"))
@@ -22,7 +22,7 @@ def person_page():
     return redirect(url_for("person._list"))
 
 
-# 만든이 페이지
-@bp.route("/creator")
-def about_page():
-    return redirect(url_for("creator._list"))
+# 게시판
+@bp.route("/question/list")
+def question_page():
+    return redirect(url_for("question._list"))
